ea67a81cfb8063244c281bdcd1a5d222358e1eee
Midam <rkdckdgns97@gmail.com>
2025-01-10 21:33:49 +0900

    framework

---
+++ TextRpg/TextRpg/Framework.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+
+
+#include "PlayerCharacter.h"
+
+
+#include "Item.h"
+#include "HealthPotion.h"
+#include "AttackBoost.h"
+
+
+#include "BaseMonster.h"
+#include "Orc.h"
+#include "Goblin.h"
+#include "EnemyFactory.h"
+
+
+#include "IBattleManager.h"
+#include "BattleManager_ver1.h"
+#include "BattleManager_ver2.h"
+
+
+#include "IState.h"
+#include "EnragedState.h"
+#include "NormalState.h"
+
+
+#include "GameManager.h"
+
+
+
+
+
+
+
