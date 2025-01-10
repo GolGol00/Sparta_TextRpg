b86cad96b3a7c152c68db99edd9a01f46e71dc7e
Dongry-96 <kevin92854654321@gamil.com>
2025-01-10 19:26:23 +0900

    add_상태 패턴 기능 추가 및 기존 코드 수정

---
+++ TextRpg/TextRpg/NormalState.cpp
@@ -0,0 +1,10 @@
+#include "NormalState.h"
+#include "BaseMonster.h"
+#include <iostream>
+
+using namespace std;
+
+void NormalState::HandleState(BaseMonster* Monster)
+{
+	// NormalState의 경우 구현 없음.
+}
--- TextRpg/TextRpg/Orc.cpp
+++ TextRpg/TextRpg/Orc.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-// 
+
 void Orc::UseSkill()
 {
-	cout << GetName() << "이(가) 분노 상태로 돌입하여 잃은 체력의 절반을 회복합니다." << endl;
+	cout << GetName() << "이(가) 잃은 체력의 절반을 회복합니다." << endl;
 	CurrentHP += (MaxHP - CurrentHP) / 2;
 }
