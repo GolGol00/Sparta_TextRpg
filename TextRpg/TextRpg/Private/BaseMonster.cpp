a23941a845560c063352e9e0f8de40b06c6ae0b7
Midam <rkdckdgns97@gmail.com>
2025-01-10 17:26:20 +0900

    정리 (플레이어 및 몬스터)

---
+++ TextRpg/TextRpg/Private/BaseMonster.cpp
@@ -0,0 +1,39 @@
+#include "BaseMonster.h"
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+BaseMonster::BaseMonster(const string& name, const int& PlayerLevel)
+{
+	Name = name;
+	Calculate_HPandDamage(PlayerLevel);
+	CurrentHP = MaxHP;
+	IsDead = false;
+}
+
+// MaxHP와 Damage 초기화 메서드
+void BaseMonster::Calculate_HPandDamage(const int& PlayerLevel)
+{
+	//srand(time(0)); 추후 메인함수에서 한 번만 사용
+	int HP_Multiplier = (rand() % 11) + 20;
+	int Damage_Multiplier = (rand() % 6) + 5;
+
+	MaxHP = PlayerLevel * HP_Multiplier;
+	Damage = PlayerLevel * Damage_Multiplier;
+}
+
+// 데미지 적용 메서드
+void BaseMonster::TakeDamage(const int& DamagedAmount)
+{
+	CurrentHP -= DamagedAmount;
+	if (CurrentHP < 0)
+	{
+		CurrentHP = 0;
+		IsDead = true;
+		return;
+	}
+}
